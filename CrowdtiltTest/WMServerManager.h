3277645f7543cb46e6f2f9c3c1e31b294e95865e
weston.mcbride <weston.mcbride@gmail.com>
2013-02-02 16:17:17 -0800

    added all the server calls to the header file

--- CrowdtiltTest/WMServerManager.h
+++ CrowdtiltTest/WMServerManager.h
@@ -22,12 +22,63 @@
 
 + (WMServerManager *)getServerManager;
 
-// Post methods with dictionaries
+#warning Weston - notes:
+/*
+ - finish adding all the server calls
+ - add custom objects to the various calls
+ - hook up server responses for non-delegate methods
+*/
+ 
+// POST methods with dictionaries
+// * User
 - (void)postUserWithDict:(NSDictionary *)dict;
+- (void)getUserAuthenticationWithDict:(NSDictionary *)dict;
+- (void)postUserCardWithDict:(NSDictionary *)dict;
+- (void)postUserBankWithDict:(NSDictionary *)dict;
+// * Campaign
 - (void)postCampaignWithDict:(NSDictionary *)dict;
+- (void)postCampaignPaymentWithDict:(NSDictionary *)dict;
+- (void)postCampaignRefundWithDict:(NSDictionary *)dict;
+- (void)postCampaignCommentWithDict:(NSDictionary *)dict;
 
-// List getter methods complete with delegates to refresh View Controllers
+// PUT methods with dictionaries (and probably need delegates to refresh custom objects)??
+// * User
+- (void)putUserWithDict:(NSDictionary *)dict;
+- (void)putUserCardWithDict:(NSDictionary *)dict;
+- (void)putUserBankWithDict:(NSDictionary *)dict;
+// * Campaign
+- (void)putCampaignWithDict:(NSDictionary *)dict;
+- (void)putCampaignPaymentWithDict:(NSDictionary *)dict;
+- (void)putCampaignSettlementBankWithDict:(NSDictionary *)dict;
+- (void)putCampaignCommentWithDict:(NSDictionary *)dict;
+
+// GET methods complete with delegates to refresh View Controllers
+// * User
 - (void)getUsersWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getSingleUserWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserCampaignsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserSingleCampaignWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserPaidCampaignsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserSingleCardWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserCardsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserSingleBankWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserBanksWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getUserPaymentsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+
+// * Campaign
 - (void)getCampaignsWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getSingleCampaignWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignSinglePaymentWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignPaymentsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignRejectedPaymentsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignSettlementsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignSingleSettlementWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignCommentsWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+- (void)getCampaignSingleCommentWithDict:(NSDictionary *)dict andWithDelegate:(NSObject <WMCrowdTiltDelegateProtocol>*)delegate;
+
+// DELETE methods
+- (void)deleteUserCardWithDict:(NSDictionary *)dict;
+- (void)deleteUserBankWithDict:(NSDictionary *)dict;
+- (void)deleteCampaignCommentWithDict:(NSDictionary *)dict;
 
 @end
